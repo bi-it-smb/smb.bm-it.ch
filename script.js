a3917501353d31586ac1ff2dfe9c58950da20efe
Benjamin Smith <benjamin.smith@EDU.LAN>
2024-10-23 16:22:28 +0200

    Comment and Naming overhaul

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+/* Retrieves the HTML element with the ID "themeToggle" and assigns it to the variable "themeToggle" */
 const themeToggle = document.getElementById('themeToggle');
+
+/* Assigns the <body> element to the variable "body" */
 const body = document.body;
 
+/* Toggles the theme with a click */
 themeToggle.addEventListener('click', () => {
     body.classList.toggle('future-mode');
-    
+
+/* Uses future-mode if it was the last used theme else default */
     if (body.classList.contains('future-mode')) {
         localStorage.setItem('theme', 'future-mode');
     } else {
@@ -11,6 +16,7 @@ themeToggle.addEventListener('click', () => {
     }
 });
 
+/* Remembers the last used theme */
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
     body.classList.add(currentTheme);
